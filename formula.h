7586e6e34c2a94ead1f6278e93f56a7444ccc5e8
neurosim <ysm1030@gmail.com>
2021-08-21 18:59:18 -0700

    Revert "Add files via upload"
    This reverts commit 60e838b6a55d8b219943a90fb018abab17d1a191.

--- formula.h
+++ formula.h
@@ -3,7 +3,7 @@
 * School of Electrical, Computer and Energy Engineering, Arizona State University
 * PI: Prof. Shimeng Yu
 * All rights reserved.
-* 
+*   
 * This source code is part of NeuroSim - a device-circuit-algorithm framework to benchmark 
 * neuro-inspired architectures with synaptic devices(e.g., SRAM and emerging non-volatile memory). 
 * Copyright of the model is maintained by the developers, and the model is distributed under 
@@ -11,14 +11,14 @@
 * http://creativecommons.org/licenses/by-nc/4.0/legalcode.
 * The source code is free and you can redistribute and/or modify it
 * by providing that the following conditions are met:
-* 
+*   
 *  1) Redistributions of source code must retain the above copyright notice,
-*     this list of conditions and the following disclaimer.
-* 
+*     this list of conditions and the following disclaimer. 
+*   
 *  2) Redistributions in binary form must reproduce the above copyright notice,
 *     this list of conditions and the following disclaimer in the documentation
 *     and/or other materials provided with the distribution.
-* 
+*   
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -31,52 +31,26 @@
 * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 * 
 * Developer list: 
-*   Pai-Yu Chen	    Email: pchen72 at asu dot edu 
-*                    
+*   Pai-Yu Chen     Email: pchen72 at asu dot edu 
+*                     
 *   Xiaochen Peng   Email: xpeng15 at asu dot edu
 ********************************************************************************/
 
-#ifndef NEUROSIM_FORMULA_H_
-#define NEUROSIM_FORMULA_H_
+#ifndef FORMULA_H_
+#define FORMULA_H_
 
-#include "Technology.h"
+#include <vector>
 
-#define MAX(a,b) (((a)> (b))?(a):(b))
-#define MIN(a,b) (((a)< (b))?(a):(b))
+double sigmoid(double x);
+double truncate(double x, int numBit, double threshold=0.5);
+double round_th(double x, double threshold);
+double NonlinearWeight(double xPulse, int maxNumLevel, double A, double B, double minConductance);
+double InvNonlinearWeight(double conductance, int maxNumLevel, double A, double B, double minConductance);
+double MeasuredLTP(double xPulse, int maxNumLevel, std::vector<double>& dataConductanceLTP);
+double MeasuredLTD(double xPulse, int maxNumLevel, std::vector<double>& dataConductanceLTD);
+double InvMeasuredLTP(double conductance, int maxNumLevel, std::vector<double>& dataConductanceLTP);
+double InvMeasuredLTD(double conductance, int maxNumLevel, std::vector<double>& dataConductanceLTD);
+double getParamA(double NL);
+double NonlinearConductance(double C, double NL, double Vw, double Vr, double V);
 
-/* Calculate MOSFET gate capacitance */
-double CalculateGateCap(double width, Technology tech);
-
-double CalculateGateArea(
-		int gateType, int numInput,
-		double widthNMOS, double widthPMOS,
-		double heightTransistorRegion, Technology tech,
-		double *height, double *width);
-
-/* Calculate the capacitance of a logic gate */
-void CalculateGateCapacitance(
-		int gateType, int numInput,
-		double widthNMOS, double widthPMOS,
-		double heightTransistorRegion, Technology tech,
-		double *capInput, double *capOutput);
-
-double CalculateDrainCap(
-		double width, int type,
-		double heightTransistorRegion, Technology tech);
-
-double CalculateGateLeakage(
-		int gateType, int numInput,
-		double widthNMOS, double widthPMOS,
-		double temperature, Technology tech);
-
-double CalculateOnResistance(double width, int type, double temperature, Technology tech);
-
-double CalculateTransconductance(double width, int type, Technology tech);
-
-double horowitz(double tr, double beta, double rampInput, double *rampOutput);
-
-double CalculatePassGateArea(double widthNMOS, double widthPMOS, Technology tech, int numFold, double *height, double *width);
-
-double NonlinearResistance(double R, double NL, double Vw, double Vr, double V);
-
-#endif /* FORMULA_H_ */
+#endif
